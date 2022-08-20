195d8ed626746b2f69e2d4799794b1b3ae85b558
Séba <sebmeloni59@gmail.com>
2022-08-20 09:58:19 +0200

    Mise au bon format des scripts d'init et d'installation.

--- scripts/Installation.sh
+++ scripts/Installation.sh
@@ -1,6 +1,25 @@
-#! /bin/bash
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+#==============================================================================
+# Titre            : Installation.sh
+# Description      : Script de lancement de l'installation compete du master
+# Auteur           : Sebastux
+# Date             : 20/08/2022
+# Modification     : 20/08/2022
+# Version          : 1.00
+# Utilisation      : ./installation.sh
+# Notes            : Script lancé par make.
+# Version de bash  : 5.1.16(1)-release
+#==============================================================================
 
 clear
+
+# shellcheck source=/dev/null
 source .venv/bin/activate
+
+# Lancement de l'installation complete du master.
 ansible-playbook site.yml -i production
+
+# Sortie de l'environnement virtuel
 deactivate
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,9 +1,33 @@
-#! /bin/bash
+#!/bin/bash
+# -*- coding: utf-8 -*-
 
+#==============================================================================
+# Titre            : init.sh
+# Description      : Script de création d'un environement virtuel
+# Auteur           : Sebastux
+# Date             : 20/08/2022
+# Modification     : 20/08/2022
+# Version          : 1.00
+# Utilisation      : ./init.sh
+# Notes            : Script lancé par make.
+# Version de bash  : 5.1.16(1)-release
+#==============================================================================
+
+# Suppression de l'ancien environnement virtuel
 rm -fr .venv
 clear
+
+# Création et mise à jour des packages par défaut.
 python3 -m venv .venv --upgrade-deps
+
+# shellcheck source=/dev/null
 source .venv/bin/activate
+
+# Ajout du package permettant la construction des autres packages.
 pip install wheel
+
+# Installation d'ansible.
 pip install ansible
+
+# Sortie de l'environnement virtuel
 deactivate
