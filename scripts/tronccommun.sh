7f369c9a77bdc1eab50e0efc0399e5b5b8e050b3
Séba <sebmeloni59@gmail.com>
2022-08-22 21:54:42 +0200

    Création du script pour l'installation du tronc commun

---
+++ scripts/tronccommun.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+#==============================================================================
+# Titre            : durcissement.sh
+# Description      : Script de lancement du durcissement de l'O.S.
+# Auteur           : Sebastux
+# Date             : 21/08/2022
+# Modification     : 21/08/2022
+# Version          : 1.00
+# Utilisation      : playbooks/durcissement.sh
+# Notes            : Script lancé par make.
+# Version de bash  : 5.1.16(1)-release
+#==============================================================================
+
+clear
+
+# shellcheck source=/dev/null
+source .venv/bin/activate
+
+# Lancement de l'installation complete du master.
+ansible-playbook playbooks/tronccommun.yml -i staging
+
+# Sortie de l'environnement virtuel
+deactivate
